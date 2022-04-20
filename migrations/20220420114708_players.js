f08d7f8c41c07fce1660944de46daac1df1fccc3
Joshua Levy <joshualevy@Joshua-Levy-MacBook-Pro.local>
2022-04-20 08:23:48 -0600

    "Updated migration and seed with base data"

--- migrations/20220420114708_players.js
+++ migrations/20220420114708_players.js
@@ -1,15 +1,13 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function (knex) {
-
+exports.up = function (knex, Promise) {
+    return knex.schema.createTable('players', players => {
+        players.increments();
+        players.string('first_name');
+        players.string('last_name');
+        players.string('nationality');
+        players.date('birthdate');
+    });
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function (knex) {
-
+exports.down = function (knex, Promise) {
+    return knex.schema.dropTableIfExists('players');
 };
--- migrations/20220420114715_tournaments.js
+++ migrations/20220420114715_tournaments.js
@@ -1,15 +1,12 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function (knex) {
-
+exports.up = function (knex, Promise) {
+    return knex.schema.createTable('tournaments', tournaments => {
+        tournaments.increments();
+        tournaments.string('name');
+        tournaments.string('location');
+        tournaments.string('category');
+    });
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function (knex) {
-
+exports.down = function (knex, Promise) {
+    return knex.schema.dropTableIfExists('tournaments');
 };
